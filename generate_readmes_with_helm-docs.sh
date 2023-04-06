3689d0531bc8af68625742b36864e3b4402eb3f2
Johannes Kastl <kastl@b1-systems.de>
2023-04-06 08:56:36 +0200

    generate_readmes_with_helm-docs.sh: remove --ignore-file, that is not needed
    - for single charts it makes no sense
    - when called without a chart as argument, it automatically takes the
      .helmdocsignore in the top-level directory of the repository

--- generate_readmes_with_helm-docs.sh
+++ generate_readmes_with_helm-docs.sh
@@ -45,7 +45,6 @@ fi
 # Run helm-docs for charts
 # 20230405 use binary from k8s-at-home fork
 helm-docs-k8s-at-home \
-    --ignore-file="${repository}/.helmdocsignore" \
     --template-files="${readme_template}" \
     --template-files="${readme_config_template}" \
     --template-files="${readme_changelog_template}" \
